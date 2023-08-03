420bf2e70672d7b8fe37014dd475925755de7449
junho <yunjunhojj@gmail.com>
2023-08-03 22:11:40 +0900

    querySelectorAll, NodeList

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// document.body.innerHTML = "<h1>Hello from JavaScript</h1>";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// 1. document.createElement("태그명")
+const h1 = document.createElement("h1");
 
-setupCounter(document.querySelector('#counter'))
+// 텍스트 추가
+h1.innerText = "Hello from JavaScript";
+
+// 스타일 추가
+h1.style.color = "red";
+h1.style.textAlign = "center";
+
+// 클래스 추가
+h1.classList.add("heading");
+// 클래스 제거
+h1.classList.remove("heading");
+
+// 2. document.body.appendChild(요소)
+// document.body.appendChild(h1);
+document.body.prepend(h1);
+
+// 3. document.body.insertBefore(요소, 기준요소)
+const h2 = document.createElement("h2");
+h2.innerText = "goodBye from JavaScript";
+document.body.insertBefore(h2, h1);
+
+// 4. document.querySelector(선택자)
+// const heading = document.querySelector(".heading");
+// const heading = document.querySelector("#heading");
+// const heading = document.querySelector("h1");
+
+// 5. document.querySelectorAll(선택자)
+// const heading = document.querySelectorAll(".heading");
+// const heading = document.querySelectorAll("#heading");
+// const heading = document.querySelectorAll("h1");
