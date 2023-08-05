d613fcdcffc789e658b2df8c9725a8afada91743
junho <yunjunhojj@gmail.com>
2023-08-05 23:46:47 +0900

    blank 기본 폴더 추가

---
+++ blank/src/main.js
@@ -0,0 +1,7 @@
+document.querySelector("#app").innerHTML = `
+  <div class="container mx-auto py-8">
+    <h1 class="text-5xl font-mono font-extrabold">
+      <span class="text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Hello!</span>
+    </h1>
+  </div>
+`;
