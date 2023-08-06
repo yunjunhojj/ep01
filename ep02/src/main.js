449eab989519ce59f21bf961cd63fffde185dec1
junho <yunjunhojj@gmail.com>
2023-08-06 16:00:07 +0900

    + - 버튼 클릭시 해당하는 product 데이터 가져오기

--- ep02/src/main.js
+++ ep02/src/main.js
@@ -15,28 +15,54 @@ const getProducts = async () => {
   return products;
 };
 
+const findElement = (startingElement, selector) => {
+  let currentElement = startingElement;
+  while (!currentElement.matches(selector)) {
+    currentElement = currentElement.parentElement;
+  }
+  return currentElement;
+};
+
 async function main() {
   const products = await getProducts();
 
   document.querySelector("#products").innerHTML = products
-    .map((product) => {
+    .map((product, index) => {
       const { name, images } = product;
       return `
-      <div class="product">
+      <div class="product" data-product-id="${
+        product.id
+      }" data-product-index="${index}">
         <img src="${images[0]}" alt="${name}" />
         <div class="product__name">${name}</div>
         <div class="product__buttons flex items-center justify-between">
           <div class="product__price">₩${product.regularPrice.toLocaleString()}</div>
           <div>
-            <button class="product__button bg-green-200 hover:bg-green-3 py-1 px-3 ">-</button>
+            <button class="product__button-decrease bg-green-200 hover:bg-green-3 py-1 px-3 ">-</button>
             <span class="product__button__divider">3</span>
-            <button class="product__button bg-green-200 hover:bg-green-3 py-1 px-3 ">+</button>
+            <button class="product__button-increase bg-green-200 hover:bg-green-3 py-1 px-3 ">+</button>
           </div>
         </div>
       </div>
     `;
     })
     .join("");
+
+  document.querySelector("#products").addEventListener("click", (event) => {
+    const { target } = event;
+    const productElement = findElement(target, ".product");
+    const productIndex = productElement.dataset.productIndex;
+    const product = products[productIndex];
+
+    if (target.classList.contains("product__button-decrease")) {
+      console.log("decrease");
+      console.log(product);
+    }
+    if (target.classList.contains("product__button-increase")) {
+      console.log("increase");
+      console.log(product);
+    }
+  });
 }
 
 main();
